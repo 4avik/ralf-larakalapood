43efbb185181967be8cae69125e32a6d6c805324
Aavik <aavik@Meow.4avik>
2022-11-17 12:25:38 +0200

    tailwindi config faili lisamine ja blade

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ //add paths to template files
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
